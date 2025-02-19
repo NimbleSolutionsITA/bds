f4c0f970de285d3cb4d72b9bf9b4e1c559ed6e60
Maxime Di Chiara <maxime.dichiara@nimble-solutions.com>
2025-02-19 12:04:28 +0100

    bug fix

--- src/utils/wordpress_api.ts
+++ src/utils/wordpress_api.ts
@@ -215,7 +215,6 @@ export const getAllProductsIds = async () => {
 
 	while (true) {
 		const response = await fetch(`${WORDPRESS_API_ENDPOINT}/product?per_page=10&page=${page}&_fields=lang,slug`);
-		console.log(response);
 		if (!response.ok) {
 			if (response.status === 400) {
 				break;
